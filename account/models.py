41333a8e52b1f0f24fd78dfb4a2036da939d7fbc
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-24 16:12:10 +0100

    Created Customer Model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    email = models.CharField(max_length=70, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
