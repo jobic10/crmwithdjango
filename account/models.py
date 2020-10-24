e48fa4112d8137f4614a7ce45686662ad16063fe
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-24 16:13:06 +0100

    Changed Model Type

--- account/models.py
+++ account/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50, null=True)
     phone = models.CharField(max_length=15, null=True)
-    email = models.CharField(max_length=70, null=True)
+    email = models.EmailField(null=True)
     date_created = models.DateTimeField(auto_now_add=True)
