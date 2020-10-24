d7bcf69d45bfbe9ff8f2748f55b90145a5e78459
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-24 13:24:48 +0100

    Template setup

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    # path('', include('account.urls')),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/', views.customer, name='customer'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    context = {}
+    return render(request, "account/dashboard.html", context)
+
+
+def products(request):
+    context = {}
+    return render(request, "account/products.html", context)
+
+
+def customer(request):
+    context = {}
+    return render(request, "account/customer.html", context)
