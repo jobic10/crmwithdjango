050663f5e8d53f50f12b9a1567fa87727395b3bf
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-24 16:13:59 +0100

    Dunder Method For Created Records

--- account/models.py
+++ account/models.py
@@ -8,3 +8,6 @@ class Customer(models.Model):
     phone = models.CharField(max_length=15, null=True)
     email = models.EmailField(null=True)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
