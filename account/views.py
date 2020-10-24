a8eab674eae44d1b598427ee19bbe76122fb90d7
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-24 20:07:02 +0100

    Passing data from views

--- account/views.py
+++ account/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def home(request):
-    context = {}
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending
+    }
     return render(request, "account/dashboard.html", context)
 
 
 def products(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, "account/products.html", context)
 
 
-def customer(request):
+def customer(request, customer_id):
     context = {}
     return render(request, "account/customer.html", context)
