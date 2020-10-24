599672fcc97d70477482d1c6ac3b0e528aeb8a24
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-24 16:40:00 +0100

    Database Models

--- account/models.py
+++ account/models.py
@@ -11,3 +11,36 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    CATEGORY = [
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+    ]
+    name = models.CharField(max_length=50, null=True)
+    price = models.FloatField()
+    category = models.CharField(max_length=50, null=True, choices=CATEGORY)
+    description = models.TextField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = [
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    ]
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=18, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag)
