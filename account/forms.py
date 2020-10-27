1c027ff44b50dbcd261a41083fd0fd818d74dd18
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-27 16:45:13 +0100

    Formatted form with bootstrap

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,11 @@ from .models import *
 
 
 class OrderForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Order
         fields = '__all__'
