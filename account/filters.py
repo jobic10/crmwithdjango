2d8db0a2e9e72ef4dba3c05a94b726ca9ceb8cc1
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-29 10:50:56 +0100

    Added note to Order model

--- account/filters.py
+++ account/filters.py
@@ -1,8 +1,13 @@
 from .models import *
 import django_filters
+from django_filters import DateFilter, CharFilter
 
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name='date_created', lookup_expr='gte')
+    end_date = DateFilter(field_name='date_created', lookup_expr='lte')
+    note = CharFilter(field_name='note', lookup_expr='icontains')
+
     class Meta:
         model = Order
-        fields = '__all__'
+        exclude = ['customer', 'date_created']
