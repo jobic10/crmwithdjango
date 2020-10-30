88c0769590e80179b6a9a906eea5d0d4904028cf
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-30 12:40:20 +0100

    Admin only decorator

--- account/decorators.py
+++ account/decorators.py
@@ -24,3 +24,16 @@ def allowed_users(allowed_roles=[]):
                 return HttpResponse("You are not authorized to view this page!")
         return wrapper_func
     return decorator
+
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'customer':
+            return redirect(reverse('userpage'))
+
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
--- account/views.py
+++ account/views.py
@@ -13,7 +13,7 @@ from .decorators import *
 
 
 @login_required
-@allowed_users(allowed_roles=['admin'])
+@admin_only
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
