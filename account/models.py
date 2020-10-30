779c040252329b9b8e8a8fa98999f2f45df2c213
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-30 14:37:34 +0100

    Added profile profile_pic to customer model

--- account/models.py
+++ account/models.py
@@ -8,6 +8,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=50, null=True)
     phone = models.CharField(max_length=15, null=True)
     email = models.EmailField(null=True)
+    profile_pic = models.ImageField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
