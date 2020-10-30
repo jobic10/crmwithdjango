758a1f2a497b20a1639d364fc6af5101574c99c6
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-30 14:02:51 +0100

    Edited Customer Model

--- account/models.py
+++ account/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null=True)
     phone = models.CharField(max_length=15, null=True)
     email = models.EmailField(null=True)
