46ed9580f046b3b514e631f6e4c29418090e2fba
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-31 15:18:04 +0100

    Switched to Postgres

--- crmwithdjango/settings.py
+++ crmwithdjango/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'crmwithdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CRM',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
