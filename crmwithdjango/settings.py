282e222a6831b2155e348023b1ee0eb8ee1ed0c1
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-31 14:36:49 +0100

    Password reset

--- crmwithdjango/settings.py
+++ crmwithdjango/settings.py
@@ -135,3 +135,10 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
